5aaf88ec568c6f90a7026c61d618806d952c119e
Josh Britain <50422789+jbritain@users.noreply.github.com>
2025-06-02 21:49:36 +0100

    make water fog less stupid

--- shaders/lib/water/waterFog.glsl
+++ shaders/lib/water/waterFog.glsl
@@ -18,8 +18,8 @@
 #ifndef WATER_FOG_GLSL
 #define WATER_FOG_GLSL
 
-#define WATER_ABSORPTION (vec3(0.3, 0.08, 0.1))
-#define WATER_SCATTERING (vec3(0.01, 0.02, 0.05) * (isEyeInWater == 1 ? vec3(0.5, 2.0, 0.5) : vec3(0.01)))
+#define WATER_ABSORPTION (vec3(1.0, 0.08, 0.1) * (isEyeInWater == 1 ? vec3(0.2) : vec3(1.0)))
+#define WATER_SCATTERING (vec3(0.0, 0.02, 0.05) * (isEyeInWater == 1 ? vec3(0.5, 2.0, 0.5) : vec3(0.01)))
 #define WATER_DENSITY 1.0
 
 vec3 waterExtinction = clamp01(WATER_ABSORPTION + WATER_SCATTERING);
@@ -36,9 +36,6 @@ vec3 waterFog(vec3 color, vec3 a, vec3 b, float dhFactor){
 
   vec3 scatter = (sunVisibilitySmooth * luminance(sunlightColor) * henyeyGreenstein(0.7, dot(normalize(b - a), lightDir)) + (EBS.y * 0.8 + 0.2) * skylightColor);
   scatter *= (1.0 - transmittance) * (WATER_SCATTERING / waterExtinction);
-
-  transmittance.r = 0.0;
-  scatter.r = 0.0;
   
 
   return color * transmittance + scatter;
